c80af7132d35a20862096305f82ba6318cd56b08
Florian Dupeyron <florian.dupeyron@mugcat.fr>
2022-05-26 11:31:37 +0200

    Modify build script to show occupied memory regions

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,6 +5,8 @@ export STM32_CUBE_G0_PATH=/project/ext/STM32CubeG0
 export STM32_TOOLCHAIN_PATH=/usr
 export STM32_TARGET_TRIPLET=arm-none-eabi
 
+arm-none-eabi-gcc --version
+
 cd /build
 
 cmake -GNinja /project
